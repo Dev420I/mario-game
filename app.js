a4246c7cabf7f794ab7549ab83b837472d479d4a
Dav420I <david.arutyunyan.97@bk.ru>
2022-11-09 00:55:58 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+kaboom({
+  global:true,
+  fullscreen: true,
+  scale:1,
+  debug:true,
+  clearColor:[0,0,0,1]
+})
+
+loadRoot('https://i.imgur.com/')
+loadSprite('coin', 'wbKxhcd.png')
+loadSprite('evil-shroom', 'KP03fR9.png' )
+loadSprite('brick', 'pogC9x5.png')
+
+
+
+
+
+scene('game', () => {
+  layers(['bg', 'obj', 'ui'], 'obj')
+
+})
+
+start('game')
